0b3d96f92ca71b8a6fe106525ee1afc6d00a0eb0
Tad-z <david.akintade1000@gmail.com>
2025-01-17 13:02:00 +0100

    session

--- src/interface/index.ts
+++ src/interface/index.ts
@@ -15,7 +15,7 @@ export type DecodedTokenInt = {
     email: string,
   }
 
-  export type NotificationInt = {
+export type NotificationInt = {
     _id: Schema.Types.ObjectId;
     userId: Schema.Types.ObjectId;
     title: string;
@@ -23,7 +23,34 @@ export type DecodedTokenInt = {
     readStatus: boolean
   };
 
-  export type NotificationDataInt = {
+export type NotificationDataInt = {
     title: string;
     body: string;
   };
+
+export type SessionInt = {
+    userId: Schema.Types.ObjectId; // Reference to the user
+    name: string; // Name of the client
+    age: number; // Age of the client
+    contact: string; // Contact number of the client
+    residence: string; // Residence of the client
+    maritalStatus: maritalStatusInt; // Enum for marital status
+    reason: reasonInt; // Enum for reason of session
+    additionalInfo?: string; // Optional field for additional case details
+    date: Date; // Date of the session
+    startTime: string; // Start time of the session
+    endTime: string; // End time of the session
+  }
+
+export enum maritalStatusInt {
+    SINGLE = "Single",
+    MARRIED = "Married",
+    DIVORCED = "Divorced",
+  }
+
+export enum reasonInt {
+    PERSONAL = "Personal Growth",
+    RELATIONSHIP = "Relationship Issues",
+    EDUCATION = "Educational Issues",
+    OTHERS = "Others",
+  }
---
+++ src/models/session.ts
@@ -0,0 +1,63 @@
+import mongoose, { Model, Schema } from "mongoose";
+import { SessionInt, maritalStatusInt, reasonInt } from "../interface";
+
+// Create a Mongoose schema
+const sessionSchema: Schema<SessionInt> = new mongoose.Schema(
+  {
+    userId: {
+      type: Schema.Types.ObjectId,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    age: {
+      type: Number,
+      required: true,
+    },
+    contact: {
+      type: String,
+      required: true,
+    },
+    residence: {
+      type: String,
+      required: true,
+    },
+    maritalStatus: {
+      type: String,
+      enum: maritalStatusInt,
+      required: true,
+    },
+    reason: {
+      type: String,
+      enum: reasonInt,
+      required: true,
+    },
+    additionalInfo: {
+      type: String,
+    },
+    date: {
+      type: Date,
+      required: true,
+    },
+    startTime: {
+      type: String,
+      required: true,
+    },
+    endTime: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// Define and export the Session model
+const Session: Model<SessionInt> = mongoose.model<SessionInt>(
+  "Session",
+  sessionSchema
+);
+export default Session;
