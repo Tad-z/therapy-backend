1c8c270f902b6b70bfa42b1d2348c1fed3de4068
Tad-z <david.akintade1000@gmail.com>
2025-01-22 22:03:07 +0100

    image

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,12 +1,27 @@
 import express from 'express';
+import multer from 'multer';
 import { addUserImage, createUser, getUsers, logIn, refreshToken } from '../controller/user';
 import auth from '../Authorization/auth';
 
 const router = express.Router();
 
+const upload = multer({
+    storage: multer.memoryStorage(),
+    limits: {
+      fileSize: 5 * 1024 * 1024, // 5MB limit
+    },
+    fileFilter: (_req, file, cb) => {
+      if (file.mimetype.startsWith('image/')) {
+        cb(null, true);
+      } else {
+        cb(new Error('Not an image! Please upload an image.'));
+      }
+    }
+  });
+
 router.post('/signup', createUser);
 router.post('/login', logIn)
-router.post("/add/image", auth, addUserImage);
+router.put('/image', auth, upload.single('file'), addUserImage);
 router.post("/refresh-token", refreshToken);
 router.get("/", getUsers);
 
